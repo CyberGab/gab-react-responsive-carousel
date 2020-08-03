8d1df86e04914bb7bcc8eefeda12b3f47daa6cdf
CyberGab <cybergab@MacBook-Pro-de-CyberGab.local>
2020-08-03 17:18:33 +0200

    first commit

---
+++ .babelrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+    presets: [
+        [
+            '@babel/env',
+            {
+                modules: 'commonjs',
+                targets: ['last 2 versions', 'not dead'],
+            },
+        ],
+        '@babel/preset-react',
+        '@babel/preset-typescript',
+    ],
+    plugins: ['@babel/plugin-proposal-class-properties'],
+};
